40129af66ce9a610fa6dac158457c299f8153638
fernandoalvesmachado <fernando.r100k@gmail.com>
2023-10-09 23:18:01 -0400

    V5

--- portifolio/app/components/ContactButton.tsx
+++ portifolio/app/components/ContactButton.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const ContactButton = ({ icon, name, link }) => {
-    return (
-        <a href={link}
-        >
+interface ContactButtonProps {
+    icon: React.ReactNode;
+    name: string;
+    link: string;
+}
 
+const ContactButton: React.FC<ContactButtonProps> = ({ icon, name, link }) => {
+    return (
+        <a href={link}>
             <div className="group relative">
-                <button
-                    className='text-white'>
+                <button className='text-white'>
                     {icon}
                 </button>
                 <span className="absolute -top-14 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 px-3 rounded-lg border border-gray-300 bg-white py-2 text-sm font-bold shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
